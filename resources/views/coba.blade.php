23f985fff2f3ba0dd401459f2acf01e4ff2ee2b4
Lisaadinda <lisaadinda118@gmail.com>
2022-04-09 15:44:09 +0700

    commit

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    Urutan ke - {{ $ke }}
+@endsection
+    
---
+++ resources/views/urutan.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title','Urutan')
+
+@endsection('content')
+
+@section('content')
+    Urutan ke - {{ $ke }}
+@endsection
+    
