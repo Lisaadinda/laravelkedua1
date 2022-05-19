ce2b38319d4860258c15d9d284cbb97db75483b3
Lisaadinda <lisaadinda118@gmail.com>
2022-05-19 08:39:01 +0700

    commit uts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\GroupsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CobaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,17 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/coba', function () {
     return view('coba');
 });
+Route::get('/coba/{no}', [CobaController::class, 'coba']); 
+Route::get('/test', [CobaController::class, 'index']); 
+Route::get('/test/{ke}', [CobaController::class, 'urutan']);
+*/
 
-Route::get('/coba/{no}', function ($no) {
-    return 'Coba ke- '. $no;
-});
+Route::get('', [CobaController::class, 'index']);
+// Route::get('/friends', [CobaController::class, 'index']);
+// Route::get('/friends/create', [CobaController::class, 'create']);
+// Route::post('/friends', [CobaController::class, 'store']);
+// Route::get('/friends/{id}', [CobaController::class, 'show']);
+// Route::get('/friends/{id}/edit', [CobaController::class, 'edit']);
+// Route::put('/friends/{id}', [CobaController::class, 'update']);
+// Route::delete('/friends/{id}', [CobaController::class, 'destroy']);
 
-Route::get('/test', 'App\Http\Controllers\CobaController@index');
-Route::get('/test/{Ke}', 'App\Http\Controllers\CobaController@urutan');
+Route::resources([
+    'friends' => CobaController::class,
+    'groups' => GroupsController::class,
+]);
